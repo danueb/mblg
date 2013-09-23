5c4cb67da3569d28ba0465245b4c0920f401cb8d
Daniel Uebelein <thespiffyone@gmail.com>
2013-09-23 13:22:59 -0500

    basic movie/movies view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+  // MODELS //
+
   window.Movie = Backbone.Model.extend({
 
   });
@@ -18,4 +21,41 @@ $(document).ready(function() {
 
   window.movs = new window.Movies();
   window.psts = new window.Posts();
+
+  // VIEWS //
+
+  window.MovieView = Backbone.View.extend({
+    className: 'movie',
+    template: Handlebars.compile( $('#movie-template').html() ),
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function() {
+      $(this.el).html( this.template( this.model.toJSON() ) );
+      return this;
+    }
+  });
+
+  window.MoviesViews = Backbone.View.extend({
+    className: 'movies',
+    template: Handlebars.compile( $('#movies-template').html() ),
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.listenTo(this.collection, 'reset', this.render);
+    },
+
+    render: function() {
+      $this = $(this.el);
+      this.collection.each(function(movie){
+        var view = new MovieView({ model: movie });
+        $this.append(view.render().el);
+      });
+      return this;
+    }
+  });
+
 });
