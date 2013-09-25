fdcde74b5030d4e8770172af8a2f15cff646b4ea
Daniel Uebelein <thespiffyone@gmail.com>
2013-09-25 17:10:55 -0500

    basic view elements added to actual page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,14 +4,35 @@ $(document).ready(function() {
 
   window.Movie = Backbone.Model.extend({
 
+    initialize: function() {
+      var imdbURL = "http://www.omdbapi.com/?i=" + this.get('imdb_id');
+      this.imdb = new IMDBData({ 'url': imdbURL });
+    }
+
   });
+
+  window.IMDBData = Backbone.Model.extend({
+    url: function(){
+      return this.get('url');
+    }
+  });
+
   window.Post = Backbone.Model.extend({
+
+    initialize: function() {
+      this.set({ 'movieTitle':  this.movie().get('title') });
+    },
+
+    movie: function() {
+      return window.movies.findWhere({"id": this.get('movie_id')});
+    }
 
   });
 
   window.Movies = Backbone.Collection.extend({
     model: Movie,
-    url: '/movies'
+    url: '/movies',
+
   });
   
   window.Posts = Backbone.Collection.extend({
@@ -19,10 +40,41 @@ $(document).ready(function() {
     url: '/posts'
   });
 
-  window.movs = new window.Movies();
-  window.psts = new window.Posts();
+  window.movies = new window.Movies(window.movieData);
+  window.posts = new window.Posts(window.postData);
 
   // VIEWS //
+
+  window.HeaderView = Backbone.View.extend({
+    tagName: 'header',
+    className: 'top-nav',
+    template: Handlebars.compile( $('#header-template').html() ),
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+    },
+
+    render: function() {
+      $(this.el).html( this.template({}) );
+      return this;
+    }
+  });
+
+  window.PostView = Backbone.View.extend({
+    tagName: 'article',
+    className: 'post',
+    template: Handlebars.compile( $('#post-template').html() ),
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      // listeners?
+    },
+
+    render: function() {
+      $(this.el).html( this.template( this.model.toJSON() ) );
+      return this;
+    }
+  })
 
   window.MovieView = Backbone.View.extend({
     className: 'movie',
@@ -30,7 +82,6 @@ $(document).ready(function() {
 
     initialize: function() {
       _.bindAll(this, 'render');
-      this.listenTo(this.model, 'change', this.render);
     },
 
     render: function() {
@@ -39,23 +90,69 @@ $(document).ready(function() {
     }
   });
 
-  window.MoviesViews = Backbone.View.extend({
-    className: 'movies',
-    template: Handlebars.compile( $('#movies-template').html() ),
+  window.MovieInfoView = window.MovieView.extend({
+    tagName: 'section',
+    className: 'movie-info',
+    template: Handlebars.compile( $('#movie-info-template').html() ),
 
     initialize: function() {
-      _.bindAll(this, 'render');
-      this.listenTo(this.collection, 'reset', this.render);
+      _.bindAll(this, 'render', 'updateData');
+      this.listenTo(this.model.imdb, 'change', this.updateData);
+      this.listenTo(this.model, 'change', this.render);
+      this.model.imdb.fetch();
     },
 
-    render: function() {
-      $this = $(this.el);
-      this.collection.each(function(movie){
-        var view = new MovieView({ model: movie });
-        $this.append(view.render().el);
-      });
-      return this;
+    updateData: function() {
+      this.model.set({ 'imdb': this.model.imdb.toJSON() });
     }
   });
 
+  // window.MoviesViews = Backbone.View.extend({
+  //   className: 'movies',
+  //   template: Handlebars.compile( $('#movies-template').html() ),
+
+  //   initialize: function() {
+  //     _.bindAll(this, 'render');
+  //     this.listenTo(this.collection, 'reset', this.render);
+  //   },
+
+  //   render: function() {
+  //   }
+  // });
+
+  window.Mblg = Backbone.Router.extend({
+    routes: {
+      '': 'home'
+    },
+
+    initialize: function() {
+      var post = window.posts.last();
+      this.headerView = new HeaderView();
+      this.postView = new PostView({
+        model: post
+      });
+      this.movieInfoView = new MovieInfoView({
+        model: post.movie()
+      })
+    },
+
+    home: function() {
+      // Mom's div and Mom's favorite div
+      var $moms = $('#moms-div'), $favDiv = $moms.find('#favorite-div');
+
+      $favDiv.empty();
+
+      if (!$(".top-nav")[0]){
+        $moms.before(this.headerView.render().el);
+      }
+
+      $favDiv.append(this.postView.render().el);
+      $favDiv.append(this.movieInfoView.render().el);
+    }
+  });
+
+  $(function() {
+    window.App = new Mblg();
+    Backbone.history.start();
+  })
 });
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
+    @movies ||= File.read('app/assets/javascripts/fake_movies.json')
+    @posts ||= File.read('app/assets/javascripts/fake_posts.json')
   end
   def test
   end
