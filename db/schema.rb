6db0999745ff0c0acabd0a6e0d825a5dc1c8ff59
Daniel Uebelein <thespiffyone@gmail.com>
2013-10-08 13:37:13 -0500

    post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002151136) do
+ActiveRecord::Schema.define(version: 20131008183308) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -20,5 +20,14 @@ ActiveRecord::Schema.define(version: 20131002151136) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.integer  "movie_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["movie_id"], name: "index_posts_on_movie_id"
 
 end
